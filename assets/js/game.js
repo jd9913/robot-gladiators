be7f4d11d6a6a9eec0f06bc3ecfe437bf463063b
Jennifer <67403842+jd9913@users.noreply.github.com>
2020-08-11 14:51:08 -0700

    creation of variables initial code of game

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,23 @@
-window.alert("This is an alert!  JavaScript is running!");
+//creates a function named "fight"
+
+
+var playerName = window.prompt("What is your robot's name?"); //add a variable to store the value entered by the user
+console.log(playerName);
+
+console.log("This logs a string, good for leaving yourself a message");
+//This will do math and log 20
+
+console.log(10 + 10);
+
+console.log("Our robot's name is " + playerName); //string concatenation
+
+function fight() {
+
+    window.alert("the fight has begun!");  //name function for functionality--best practice
+
+
+
+} //code block between the braces--computer runs the code inside but not outside the braces.
+
+
+//fight(); //calls the function (tells the computer to execute it.)
