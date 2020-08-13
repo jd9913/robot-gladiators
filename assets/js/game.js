ff81061281309bc25a00a0e47d65d28f0ecaa0c0
Jennifer <67403842+jd9913@users.noreply.github.com>
2020-08-13 16:32:11 -0700

    added some while statements and added multiple robots

--- assets/js/game.js
+++ assets/js/game.js
@@ -73,7 +73,6 @@ var fight = function(enemyName) {
         else {
 
             window.alert(playerName + " still has  " + playerHealth + "health left.");
-
         };
 
         //if player chooses to skip
@@ -89,17 +88,25 @@ var fight = function(enemyName) {
             playerMoney = playerMoney - 2;
         }
 
-        //if no (false), ask question again by running fight () again
+        //if not (false), ask question again by running fight () again
         else {
-            fight();
+            var fight = function (enemyName) {
+                //repeat and execute as long as the enemy robot is alive
+                while (enemyHealth > 0) {
+                    
+                }
         }
     
-        window.alert(playerName + " has chosen to skip the fight!");
-    } else {
-        window.alert("You need to pick a valid option.  Try again!");
+            window.alert(playerName + " has chosen to skip the fight!");
+
+        } 
+
+            window.alert("You need to pick a valid option.  Try again!");
     }
 };
 
 for (var i = 0; i < enemyName.length; i++) {
+    debugger;
+    //call fight function with enemy robot
     fight(enemyName[i]);
 }
